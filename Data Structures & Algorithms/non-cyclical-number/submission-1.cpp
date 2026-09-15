5f51f4b0652610c77de31f9dca0b3a45b4fa3efa
Aiman Mumtaz <51526789+aiman-mumtaz@users.noreply.github.com>
2026-09-15 21:53:41 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int squareSum(int n){
+        int r=0,s=0;
+        while(n){
+            r = n%10;
+            n/=10;
+            s+=r*r;
+        }
+        return s;
+    }
+    bool isHappy(int n) {
+        if(n==1){
+            return true;
+        }
+        set<int> s;
+        while(s.find(squareSum(n)) == s.end()){
+            cout<<squareSum(n)<<" ";
+            n = squareSum(n);
+            s.insert(n);
+        }
+        if(n==1){
+            return true;
+        }
+        return false;
+    }
+};
